acf3590bac8fa8d1eb90b370b4dcdf835243c519
Paulo <paulopacheco7@hotmail.com>
2023-05-19 12:27:52 -0300

    atualizado

---
+++ aula04/exerc008.js
@@ -0,0 +1,6 @@
+var vel = 70.5
+console.log(`A velocidade do seu carro é ${vel} KM/H`)
+if (vel > 60) {
+    console.log(`Você ultrapassou a velocidade permitida. MULTADO`)
+}
+console.log(`Dirija sempre usando cinto de segurança`)
