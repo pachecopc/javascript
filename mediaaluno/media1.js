fccdf5546c864944c64db9980b042d8dcb214325
Paulo <120028058+pachecopc@users.noreply.github.com>
2023-06-20 09:52:27 -0300

    atualizado

--- mediaaluno/media1.js
+++ mediaaluno/media1.js
@@ -1,22 +1,10 @@
 
-const entradas =[8];
-let i = 0;
-
-function gets() {
-    const valor = entradas[i];
-    i++;
-    return valor;
-}
-
-    function print(texto) {
-        console.log(texto);
-    }
 
     const { gets, print } = require('./funcoes-auxiliares-ex1');
 
     const media = gets();
 
-    if (media < 5) {
+    if (media >= 0 && media < 5) {
         print('Reprovado');
     } else if (media >= 5 && media < 7) {
         print('Recuperação');
